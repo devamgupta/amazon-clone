a92fb1272ed0594f003fdd9a70f81a52aeb89992
Devam Gupta <devamgupta26@gmail.com>
2021-07-17 19:21:56 +0530

    Built the banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>Amazon Clone : Checkout</h1>
           </Route>
           <Route path="/login">
@@ -16,7 +18,7 @@ function App() {
           {/* This is the default route */}
           <Route path="/">
             <Header />
-            <h1>Amazon Clone : Home Page</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
