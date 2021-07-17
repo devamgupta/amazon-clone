6d5de81c201c5274f8aa18324b8c3fb157ef8164
Devam Gupta <devamgupta26@gmail.com>
2021-07-17 12:07:14 +0530

    Set up React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Amazon Clone</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Amazon Clone : Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Amazon Clone : Login</h1>
+          </Route>
+          {/* This is the default route */}
+          <Route path="/">
+            <h1>Amazon Clone : Home Page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
