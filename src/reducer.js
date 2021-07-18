568cfda7e92766843f9d39e3f0e32295ebc174f1
Devam Gupta <devamgupta26@gmail.com>
2021-07-18 17:25:50 +0530

    Setup the react context api and setup Add to basket button

---
+++ src/reducer.js
@@ -0,0 +1,22 @@
+// all the data layer logic go here
+export const initialState = {
+    basket: [],
+    user: null,
+};
+
+const reducer = (state, action) => {
+    console.log(action);
+    switch(action.type) {
+        case 'ADD_TO_BASKET':
+            return {
+                ...state,
+                basket: [...state.basket, action.item]
+            };
+        case 'REMOVE_FROM_BASKET':
+            return { state };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
